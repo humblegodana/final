e62c7dcef4c16c1ff16e81d30d87633a2e4a3a01
humblegodana <bxy0314@gmail.com>
2026-04-22 13:43:40 +0800

    Update bib.js

--- survis-master/src/data/generated/bib.js
+++ survis-master/src/data/generated/bib.js
@@ -1,17 +1,157 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
+define({ entries : {
+    "healey2005detecting": {
         "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
+        "title": "Detecting stress during real-world driving tasks using physiological sensors",
+        "author": "Healey, Jennifer A and Picard, Rosalind W",
+        "journal": "IEEE Transactions on intelligent transportation systems",
+        "volume": "6",
+        "number": "2",
+        "pages": "156--166",
+        "year": "2005",
+        "publisher": "IEEE",
+        "doi": "10.1109/TITS.2005.848368"
+    },
+    "schmidt2018introducing": {
+        "type": "inproceedings",
+        "title": "Introducing wesad, a multimodal dataset for wearable stress and affect detection",
+        "author": "Schmidt, Philip and Reiss, Attila and Duerichen, Robert and Marberger, Claus and Van Laerhoven, Kristof",
+        "booktitle": "Proceedings of the 20th ACM international conference on multimodal interaction",
+        "pages": "400--408",
+        "year": "2018",
+        "doi": "10.1145/3242969.3242985"
+    },
+    "miranda2018amigos": {
+        "type": "article",
+        "title": "Amigos: A dataset for affect, personality and mood research on individuals and groups",
+        "author": "Miranda-Correa, Juan Abdon and Abadi, Mojtaba Khomami and Sebe, Nicu and Patras, Ioannis",
+        "journal": "IEEE transactions on affective computing",
+        "volume": "12",
+        "number": "2",
+        "pages": "479--493",
+        "year": "2018",
+        "publisher": "IEEE",
+        "doi": "10.1109/TAFFC.2018.2884461"
+    },
+    "hovsepian2015cstress": {
+        "type": "inproceedings",
+        "title": "cStress: towards a gold standard for continuous stress assessment in the mobile environment",
+        "author": "Hovsepian, Karen and Al'Absi, Mustafa and Ertin, Emre and Kamarck, Thomas and Nakajima, Motohiro and Kumar, Santosh",
+        "booktitle": "Proceedings of the 2015 ACM international joint conference on pervasive and ubiquitous computing",
+        "pages": "493--504",
+        "year": "2015",
+        "doi": "10.1145/2750858.2807526"
+    },
+    "reiss2019deep": {
+        "type": "article",
+        "title": "Deep PPG: Large-scale heart rate estimation with convolutional neural networks",
+        "author": "Reiss, Attila and Indlekofer, Ina and Schmidt, Philip and Van Laerhoven, Kristof",
+        "journal": "Sensors",
+        "volume": "19",
+        "number": "14",
+        "pages": "3079",
+        "year": "2019",
+        "publisher": "MDPI",
+        "doi": "10.3390/s19143079"
+    },
+    "rashid2023stress": {
+        "type": "article",
+        "title": "Stress detection using context-aware sensor fusion from wearable devices",
+        "author": "Rashid, Nafiul and Mortlock, Trier and Al Faruque, Mohammad Abdullah",
+        "journal": "IEEE Internet of Things Journal",
+        "volume": "10",
+        "number": "16",
+        "pages": "14114--14127",
+        "year": "2023",
+        "publisher": "IEEE",
+        "doi": "10.1109/JIOT.2023.3265768"
+    },
+    "mishra2020evaluating": {
+        "type": "article",
+        "title": "Evaluating the reproducibility of physiological stress detection models",
+        "author": "Mishra, Varun and Sen, Sougata and Chen, Grace and Hao, Tian and Rogers, Jeffrey and Chen, Ching-Hua and Kotz, David",
+        "journal": "Proceedings of the ACM on interactive, mobile, wearable and ubiquitous technologies",
+        "volume": "4",
+        "number": "4",
+        "pages": "1--29",
+        "year": "2020",
+        "publisher": "ACM",
+        "doi": "10.1145/3432220"
+    },
+    "bota2019review": {
+        "type": "article",
+        "title": "A review, current challenges, and future possibilities on emotion recognition using machine learning and physiological signals.",
+        "author": "Bota, Patricia J and Wang, Chen and Fred, Ana LN and Da Silva, Hugo Pl{\\'a}cido",
+        "journal": "IEEE access",
+        "volume": "7",
+        "pages": "140990--141020",
+        "year": "2019",
+        "doi": "10.1109/ACCESS.2019.2944001"
+    },
+    "koldijk2016deriving": {
+        "type": "article",
+        "title": "Deriving requirements for pervasive well-being technology from work stress and intervention theory: framework and case study",
+        "author": "Koldijk, Saskia and Kraaij, Wessel and Neerincx, Mark A",
+        "journal": "JMIR mHealth and uHealth",
+        "volume": "4",
+        "number": "3",
+        "pages": "e5341",
+        "year": "2016",
+        "publisher": "JMIR Publications Inc.",
+        "doi": "10.2196/mhealth.5341"
+    },
+    "koldijk2015visual": {
+        "type": "inproceedings",
+        "title": "Visual analytics of work behavior data-insights on individual differences",
+        "author": "Koldijk, Saskia and Bernard, J{\\\"u}rgen and Ruppert, Tobias and Kohlhammer, J{\\\"o}rn and Neerincx, Mark A and Kraaij, Wessel",
+        "booktitle": "Eurographics Conference on Visualization (EuroVis) - Short Papers",
+        "year": "2015",
+        "publisher": "The Eurographics Association",
+        "doi": "10.2312/eurovisshort.20151129"
+    },
+    "giannakakis2019review": {
+        "type": "article",
+        "title": "Review on psychological stress detection using biosignals",
+        "author": "Giannakakis, Giorgos and Grigoriadis, Dimitris and Giannakaki, Katerina and Simantiraki, Olympia and Roniotis, Alexandros and Tsiknakis, Manolis",
+        "journal": "IEEE transactions on affective computing",
+        "volume": "13",
+        "number": "1",
+        "pages": "440--460",
+        "year": "2019",
+        "publisher": "IEEE",
+        "doi": "10.1109/TAFFC.2019.2927337"
+    },
+    "sriramprakash2017stress": {
+        "type": "article",
+        "title": "Stress detection in working people",
+        "author": "Sriramprakash, Senthil and Prasanna, Vadana D and Murthy, OV Ramana",
+        "journal": "Procedia computer science",
+        "volume": "115",
+        "pages": "359--366",
+        "year": "2017",
+        "publisher": "Elsevier",
+        "doi": "10.1016/J.PROCS.2017.09.090"
+    },
+    "hosseini2022multimodal": {
+        "type": "article",
+        "title": "A multimodal sensor dataset for continuous stress detection of nurses in a hospital",
+        "author": "Hosseini, Seyedmajid and Gottumukkala, Raju and Katragadda, Satya and Bhupatiraju, Ravi Teja and Ashkar, Ziad and Borst, Christoph W and Cochran, Kenneth",
+        "journal": "Scientific Data",
+        "volume": "9",
+        "number": "1",
+        "pages": "255",
+        "year": "2022",
+        "publisher": "Nature Publishing Group",
+        "doi": "10.1038/s41597-022-01361-y"
+    },
+    "gedam2021review": {
+        "type": "article",
+        "title": "A review on mental stress detection using wearable sensors and machine learning techniques",
+        "author": "Gedam, Shruti and Paul, Sanchita",
+        "journal": "IEEE Access",
+        "volume": "9",
+        "pages": "84045--84066",
+        "year": "2021",
+        "publisher": "IEEE",
+        "doi": "10.1109/ACCESS.2021.3085502"
     }
-};
+}});
