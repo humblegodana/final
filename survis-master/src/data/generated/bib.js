3aa80283efb40de3991a9465e348d015cc2f2e9e
humblegodana <bxy0314@gmail.com>
2026-04-22 14:18:14 +0800

    Add files via upload

--- survis-master/src/data/generated/bib.js
+++ survis-master/src/data/generated/bib.js
@@ -1,159 +1,157 @@
-define([], function() {
-    var rawBibtex = `
-@article{healey2005detecting,
-  title={Detecting stress during real-world driving tasks using physiological sensors},
-  author={Healey, Jennifer A and Picard, Rosalind W},
-  journal={IEEE Transactions on intelligent transportation systems},
-  volume={6},
-  number={2},
-  pages={156--166},
-  year={2005},
-  publisher={IEEE},
-  doi={10.1109/TITS.2005.848368}
-}
-
-@inproceedings{schmidt2018introducing,
-  title={Introducing wesad, a multimodal dataset for wearable stress and affect detection},
-  author={Schmidt, Philip and Reiss, Attila and Duerichen, Robert and Marberger, Claus and Van Laerhoven, Kristof},
-  booktitle={Proceedings of the 20th ACM international conference on multimodal interaction},
-  pages={400--408},
-  year={2018},
-  doi={10.1145/3242969.3242985}
-}
-
-@article{miranda2018amigos,
-  title={Amigos: A dataset for affect, personality and mood research on individuals and groups},
-  author={Miranda-Correa, Juan Abdon and Abadi, Mojtaba Khomami and Sebe, Nicu and Patras, Ioannis},
-  journal={IEEE transactions on affective computing},
-  volume={12},
-  number={2},
-  pages={479--493},
-  year={2018},
-  publisher={IEEE},
-  doi={10.1109/TAFFC.2018.2884461}
-}
-
-@inproceedings{hovsepian2015cstress,
-  title={cStress: towards a gold standard for continuous stress assessment in the mobile environment},
-  author={Hovsepian, Karen and AlAbsi, Mustafa and Ertin, Emre and Kamarck, Thomas and Nakajima, Motohiro and Kumar, Santosh},
-  booktitle={Proceedings of the 2015 ACM international joint conference on pervasive and ubiquitous computing},
-  pages={493--504},
-  year={2015},
-  doi={10.1145/2750858.2807526}
-}
-
-@article{reiss2019deep,
-  title={Deep PPG: Large-scale heart rate estimation with convolutional neural networks},
-  author={Reiss, Attila and Indlekofer, Ina and Schmidt, Philip and Van Laerhoven, Kristof},
-  journal={Sensors},
-  volume={19},
-  number={14},
-  pages={3079},
-  year={2019},
-  publisher={MDPI},
-  doi={10.3390/s19143079}
-}
-
-@article{rashid2023stress,
-  title={Stress detection using context-aware sensor fusion from wearable devices},
-  author={Rashid, Nafiul and Mortlock, Trier and Al Faruque, Mohammad Abdullah},
-  journal={IEEE Internet of Things Journal},
-  volume={10},
-  number={16},
-  pages={14114--14127},
-  year={2023},
-  publisher={IEEE},
-  doi={10.1109/JIOT.2023.3265768}
-}
-
-@article{mishra2020evaluating,
-  title={Evaluating the reproducibility of physiological stress detection models},
-  author={Mishra, Varun and Sen, Sougata and Chen, Grace and Hao, Tian and Rogers, Jeffrey and Chen, Ching-Hua and Kotz, David},
-  journal={Proceedings of the ACM on interactive, mobile, wearable and ubiquitous technologies},
-  volume={4},
-  number={4},
-  pages={1--29},
-  year={2020},
-  publisher={ACM},
-  doi={10.1145/3432220}
-}
-
-@article{bota2019review,
-  title={A review, current challenges, and future possibilities on emotion recognition using machine learning and physiological signals},
-  author={Bota, Patricia J and Wang, Chen and Fred, Ana LN and Da Silva, Hugo Placido},
-  journal={IEEE access},
-  volume={7},
-  pages={140990--141020},
-  year={2019},
-  doi={10.1109/ACCESS.2019.2944001}
-}
-
-@article{koldijk2016deriving,
-  title={Deriving requirements for pervasive well-being technology from work stress and intervention theory: framework and case study},
-  author={Koldijk, Saskia and Kraaij, Wessel and Neerincx, Mark A},
-  journal={JMIR mHealth and uHealth},
-  volume={4},
-  number={3},
-  pages={e5341},
-  year={2016},
-  publisher={JMIR Publications Inc},
-  doi={10.2196/mhealth.5341}
-}
-
-@inproceedings{koldijk2015visual,
-  title={Visual analytics of work behavior data-insights on individual differences},
-  author={Koldijk, Saskia and Bernard, Jurgen and Ruppert, Tobias and Kohlhammer, Jorn and Neerincx, Mark A and Kraaij, Wessel},
-  booktitle={Eurographics Conference on Visualization (EuroVis) - Short Papers},
-  year={2015},
-  publisher={The Eurographics Association},
-  doi={10.2312/eurovisshort.20151129}
-}
-
-@article{giannakakis2019review,
-  title={Review on psychological stress detection using biosignals},
-  author={Giannakakis, Giorgos and Grigoriadis, Dimitris and Giannakaki, Katerina and Simantiraki, Olympia and Roniotis, Alexandros and Tsiknakis, Manolis},
-  journal={IEEE transactions on affective computing},
-  volume={13},
-  number={1},
-  pages={440--460},
-  year={2019},
-  publisher={IEEE},
-  doi={10.1109/TAFFC.2019.2927337}
-}
-
-@article{sriramprakash2017stress,
-  title={Stress detection in working people},
-  author={Sriramprakash, Senthil and Prasanna, Vadana D and Murthy, OV Ramana},
-  journal={Procedia computer science},
-  volume={115},
-  pages={359--366},
-  year={2017},
-  publisher={Elsevier},
-  doi={10.1016/J.PROCS.2017.09.090}
-}
-
-@article{hosseini2022multimodal,
-  title={A multimodal sensor dataset for continuous stress detection of nurses in a hospital},
-  author={Hosseini, Seyedmajid and Gottumukkala, Raju and Katragadda, Satya and Bhupatiraju, Ravi Teja and Ashkar, Ziad and Borst, Christoph W and Cochran, Kenneth},
-  journal={Scientific Data},
-  volume={9},
-  number={1},
-  pages={255},
-  year={2022},
-  publisher={Nature Publishing Group},
-  doi={10.1038/s41597-022-01361-y}
-}
-
-@article{gedam2021review,
-  title={A review on mental stress detection using wearable sensors and machine learning techniques},
-  author={Gedam, Shruti and Paul, Sanchita},
-  journal={IEEE Access},
-  volume={9},
-  pages={84045--84066},
-  year={2021},
-  publisher={IEEE},
-  doi={10.1109/ACCESS.2021.3085502}
-}
-`;
-    return rawBibtex;
-});
+﻿const generatedBibEntries = {
+    "bota2019review": {
+        "author": "Bota, Patricia J and Wang, Chen and Fred, Ana LN and Da Silva, Hugo Pl{\\'a}cido",
+        "doi": "10.1109/ACCESS.2019.2944001",
+        "journal": "IEEE access",
+        "pages": "140990--141020",
+        "title": "A review, current challenges, and future possibilities on emotion recognition using machine learning and physiological signals.",
+        "type": "article",
+        "volume": "7",
+        "year": "2019"
+    },
+    "gedam2021review": {
+        "author": "Gedam, Shruti and Paul, Sanchita",
+        "doi": "10.1109/ACCESS.2021.3085502",
+        "journal": "IEEE Access",
+        "pages": "84045--84066",
+        "publisher": "IEEE",
+        "title": "A review on mental stress detection using wearable sensors and machine learning techniques",
+        "type": "article",
+        "volume": "9",
+        "year": "2021"
+    },
+    "giannakakis2019review": {
+        "author": "Giannakakis, Giorgos and Grigoriadis, Dimitris and Giannakaki, Katerina and Simantiraki, Olympia and Roniotis, Alexandros and Tsiknakis, Manolis",
+        "doi": "10.1109/TAFFC.2019.2927337",
+        "journal": "IEEE transactions on affective computing",
+        "number": "1",
+        "pages": "440--460",
+        "publisher": "IEEE",
+        "title": "Review on psychological stress detection using biosignals",
+        "type": "article",
+        "volume": "13",
+        "year": "2019"
+    },
+    "healey2005detecting": {
+        "author": "Healey, Jennifer A and Picard, Rosalind W",
+        "doi": "10.1109/TITS.2005.848368",
+        "journal": "IEEE Transactions on intelligent transportation systems",
+        "number": "2",
+        "pages": "156--166",
+        "publisher": "IEEE",
+        "title": "Detecting stress during real-world driving tasks using physiological sensors",
+        "type": "article",
+        "volume": "6",
+        "year": "2005"
+    },
+    "hosseini2022multimodal": {
+        "author": "Hosseini, Seyedmajid and Gottumukkala, Raju and Katragadda, Satya and Bhupatiraju, Ravi Teja and Ashkar, Ziad and Borst, Christoph W and Cochran, Kenneth",
+        "doi": "10.1038/s41597-022-01361-y",
+        "journal": "Scientific Data",
+        "number": "1",
+        "pages": "255",
+        "publisher": "Nature Publishing Group",
+        "title": "A multimodal sensor dataset for continuous stress detection of nurses in a hospital",
+        "type": "article",
+        "volume": "9",
+        "year": "2022"
+    },
+    "hovsepian2015cstress": {
+        "author": "Hovsepian, Karen and Al'Absi, Mustafa and Ertin, Emre and Kamarck, Thomas and Nakajima, Motohiro and Kumar, Santosh",
+        "booktitle": "Proceedings of the 2015 ACM international joint conference on pervasive and ubiquitous computing",
+        "doi": "10.1145/2750858.2807526",
+        "pages": "493--504",
+        "title": "cStress: towards a gold standard for continuous stress assessment in the mobile environment",
+        "type": "inproceedings",
+        "year": "2015"
+    },
+    "koldijk2015visual": {
+        "author": "Koldijk, Saskia and Bernard, J{\\\"u}rgen and Ruppert, Tobias and Kohlhammer, J{\\\"o}rn and Neerincx, Mark A and Kraaij, Wessel",
+        "booktitle": "Eurographics Conference on Visualization (EuroVis) - Short Papers",
+        "doi": "10.2312/eurovisshort.20151129",
+        "publisher": "The Eurographics Association",
+        "title": "Visual analytics of work behavior data-insights on individual differences",
+        "type": "inproceedings",
+        "year": "2015"
+    },
+    "koldijk2016deriving": {
+        "author": "Koldijk, Saskia and Kraaij, Wessel and Neerincx, Mark A",
+        "doi": "10.2196/mhealth.5341",
+        "journal": "JMIR mHealth and uHealth",
+        "number": "3",
+        "pages": "e5341",
+        "publisher": "JMIR Publications Inc.",
+        "title": "Deriving requirements for pervasive well-being technology from work stress and intervention theory: framework and case study",
+        "type": "article",
+        "volume": "4",
+        "year": "2016"
+    },
+    "miranda2018amigos": {
+        "author": "Miranda-Correa, Juan Abdon and Abadi, Mojtaba Khomami and Sebe, Nicu and Patras, Ioannis",
+        "doi": "10.1109/TAFFC.2018.2884461",
+        "journal": "IEEE transactions on affective computing",
+        "number": "2",
+        "pages": "479--493",
+        "publisher": "IEEE",
+        "title": "Amigos: A dataset for affect, personality and mood research on individuals and groups",
+        "type": "article",
+        "volume": "12",
+        "year": "2018"
+    },
+    "mishra2020evaluating": {
+        "author": "Mishra, Varun and Sen, Sougata and Chen, Grace and Hao, Tian and Rogers, Jeffrey and Chen, Ching-Hua and Kotz, David",
+        "doi": "10.1145/3432220",
+        "journal": "Proceedings of the ACM on interactive, mobile, wearable and ubiquitous technologies",
+        "number": "4",
+        "pages": "1--29",
+        "publisher": "ACM",
+        "title": "Evaluating the reproducibility of physiological stress detection models",
+        "type": "article",
+        "volume": "4",
+        "year": "2020"
+    },
+    "rashid2023stress": {
+        "author": "Rashid, Nafiul and Mortlock, Trier and Al Faruque, Mohammad Abdullah",
+        "doi": "10.1109/JIOT.2023.3265768",
+        "journal": "IEEE Internet of Things Journal",
+        "number": "16",
+        "pages": "14114--14127",
+        "publisher": "IEEE",
+        "title": "Stress detection using context-aware sensor fusion from wearable devices",
+        "type": "article",
+        "volume": "10",
+        "year": "2023"
+    },
+    "reiss2019deep": {
+        "author": "Reiss, Attila and Indlekofer, Ina and Schmidt, Philip and Van Laerhoven, Kristof",
+        "doi": "10.3390/s19143079",
+        "journal": "Sensors",
+        "number": "14",
+        "pages": "3079",
+        "publisher": "MDPI",
+        "title": "Deep PPG: Large-scale heart rate estimation with convolutional neural networks",
+        "type": "article",
+        "volume": "19",
+        "year": "2019"
+    },
+    "schmidt2018introducing": {
+        "author": "Schmidt, Philip and Reiss, Attila and Duerichen, Robert and Marberger, Claus and Van Laerhoven, Kristof",
+        "booktitle": "Proceedings of the 20th ACM international conference on multimodal interaction",
+        "doi": "10.1145/3242969.3242985",
+        "pages": "400--408",
+        "title": "Introducing wesad, a multimodal dataset for wearable stress and affect detection",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "sriramprakash2017stress": {
+        "author": "Sriramprakash, Senthil and Prasanna, Vadana D and Murthy, OV Ramana",
+        "doi": "10.1016/J.PROCS.2017.09.090",
+        "journal": "Procedia computer science",
+        "pages": "359--366",
+        "publisher": "Elsevier",
+        "title": "Stress detection in working people",
+        "type": "article",
+        "volume": "115",
+        "year": "2017"
+    }
+};
